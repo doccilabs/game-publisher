c9cd256f372b60679f4454ab91e1edd71474e2db
BrianDYKim <18k7102dy@naver.com>
2023-03-28 21:14:40 +0900

    add: JPA + MySQL 설정 완료

--- adapter/application-user/src/main/kotlin/com/elicepark/user/UserApplication.kt
+++ adapter/application-user/src/main/kotlin/com/elicepark/user/UserApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ApplicationUserApplication
+class UserApplication
 
 fun main(args: Array<String>) {
-    runApplication<ApplicationUserApplication>(*args)
+    runApplication<UserApplication>(*args)
 }
